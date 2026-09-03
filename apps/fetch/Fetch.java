d2bef4129120b4e2ea917c3704c56287de990816
Alex Laurie <alex.r.laurie@gmail.com>
2026-09-03 11:54:21 +1000

    perf(fetch): tune JVM options to reduce memory usage and startup time

--- apps/fetch/Fetch.java
+++ apps/fetch/Fetch.java
@@ -3,7 +3,7 @@
 //DEPS info.picocli:picocli:4.7.7
 //DEPS info.picocli:picocli-codegen:4.7.7
 //JAVAC_OPTIONS -proc:full
-//JAVA_OPTIONS --enable-native-access=ALL-UNNAMED
+//JAVA_OPTIONS --enable-native-access=ALL-UNNAMED -XX:+UseSerialGC -Xms16m -Xmx64m -XX:TieredStopAtLevel=1
 //NATIVE_OPTIONS -O2 --no-fallback
 
 package fetch;
